542f87b8dfba26235df1288c104f4ac3d9161de8
Navya Gupta <144938273+navyag3060@users.noreply.github.com>
2025-12-17 19:55:55 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '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'
+    #ENTER YOUR OPENAI KEY
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'OPENAI KEY'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///pawcare.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
